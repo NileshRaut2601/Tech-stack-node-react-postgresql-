d8aa98d7ee2906d5a2042948b07ea651daa2f8c5
NileshRaut2601 <nileshraut2601@gamil.com>
2026-09-18 21:45:27 +0530

    Learn Path module and Global Constant

---
+++ Node-2026/path.js
@@ -0,0 +1,14 @@
+const path=require('path');
+
+const file="text/Nilesh.txt";
+
+
+// path module 
+console.log(path.extname(file));
+console.log(path.dirname(file));
+console.log(path.basename(file));
+
+
+// global Constant
+console.log(__dirname);
+console.log(__filename);
